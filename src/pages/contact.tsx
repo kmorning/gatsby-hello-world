093e4fd5de4ee61e1e44d290f21da6ce8f4b7448
Kelly Morning <kleemorning@gmail.com>
2021-01-25 00:59:32 -0500

    initial commit

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Header from '../components/Header'
+
+const Contact: React.FC = () => (
+  <div style={{ color: `teal` }}>
+    <Link to="/">Home</Link>
+    <Header headerText="Contact" />
+    <p>Send us a message!</p>
+  </div>
+)
+
+export default Contact
