e4ed25a39a586f7f2872d79617db7a7cbf8a9176
Kelly Morning <kleemorning@gmail.com>
2021-01-30 17:41:05 -0500

    create navigation links

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,33 @@
+import { Link } from 'gatsby'
 import React from 'react'
 
-interface Props {
+interface ListLinkProps {
+  to: string
   children: React.ReactNode
 }
 
-const Layout: React.FC<Props> = ({ children }) => (
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const ListLink: React.FC<ListLinkProps> = ({ to, children }) => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={to}>{children}</Link>
+  </li>
+)
+
+const Layout: React.FC<LayoutProps> = ({ children }) => (
   <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-    <h3>Alcoholics Anonymous Quinte East</h3>
+    <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <h3>Alcoholics Anonymous Quinte East</h3>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about">About</ListLink>
+        <ListLink to="/contact">Contact</ListLink>
+      </ul>
+    </header>
     {children}
   </div>
 )
