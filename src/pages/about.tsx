d412d9b7db98d909b8429d8b13b053842b911d5a
Kelly Morning <kleemorning@gmail.com>
2021-01-31 12:34:41 -0500

    use page query to retrieve site metadata

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,32 @@
+import { graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../components/Layout'
 
-const About: React.FC = () => (
+interface PageQueryProps {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+      }
+    }
+  }
+}
+
+const About: React.FC<PageQueryProps> = ({ data }) => (
   <Layout>
-    <h1>About</h1>
+    <h1>{data.site.siteMetadata.title} Development</h1>
     <p>This is the staging ground for the development of our website.</p>
   </Layout>
 )
 
 export default About
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
